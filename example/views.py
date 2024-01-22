65cd6ed3922810e9788d6091dc9fe8663a7d63d9
tanveer1517 <tanveerobjects@gmail.com>
2024-01-23 01:15:12 +0500

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,16 +1,26 @@
-# example/views.py
-from datetime import datetime
-
-from django.http import HttpResponse
+# views.py
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from .forms import EmailForm
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            C_User = form.cleaned_data['C_User']
+            from_email = 'tanweerahmedzop5@gmail.com'
+            recipient_list = ['postskaka@gmail.com']
+
+            # Render the email template
+            email_content = render_to_string('email_template.html', {'subject': subject, 'message': message, 'C_User': C_User})
+
+            send_mail(subject, email_content, from_email, recipient_list, html_message=email_content)
+
+            return render(request, 'email_sent.html')
+    else:
+        form = EmailForm()
+
+    return render(request, 'email_form.html', {'form': form})
